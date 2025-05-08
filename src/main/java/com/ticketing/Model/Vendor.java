fcbc132d27f21a7044c96f8218effbd05999db3e
harinipasansha2002 <hpasansha@gmail.com>
2025-05-08 11:33:28 +0530

    Add Vendor class with name property and basic getters/setters

---
+++ src/main/java/com/ticketing/Model/Vendor.java
@@ -0,0 +1,19 @@
+package com.ticketing.Model;
+
+public class Vendor {
+    private String name;
+
+    public Vendor(){}
+
+    public Vendor(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
