88aa1c4c7adea22a539387f6fbc5fde61706be1c
harinipasansha2002 <hpasansha@gmail.com>
2025-05-08 11:22:29 +0530

    Add ticket booking system core components including models, services, and repository implementation

---
+++ src/main/java/com/ticketing/Model/Configuration.java
@@ -0,0 +1,58 @@
+package com.ticketing.Model;
+
+public class Configuration {
+    private int totalTickets;
+    private int ticketReleaseRate;
+    private int customerRetrievalRate;
+    private int maxTicketCapacity;
+    private int numberOfVendors;
+    private int numberOfCustomers;
+
+    public int getTotalTickets() {
+        return totalTickets;
+    }
+
+    public void setTotalTickets(int totalTickets) {
+        this.totalTickets = totalTickets;
+    }
+
+    public int getTicketReleaseRate() {
+        return ticketReleaseRate;
+    }
+
+    public void setTicketReleaseRate(int ticketReleaseRate) {
+        this.ticketReleaseRate = ticketReleaseRate;
+    }
+
+    public int getCustomerRetrievalRate() {
+        return customerRetrievalRate;
+    }
+
+    public void setCustomerRetrievalRate(int customerRetrievalRate) {
+        this.customerRetrievalRate = customerRetrievalRate;
+    }
+
+    public int getMaxTicketCapacity() {
+        return maxTicketCapacity;
+    }
+
+    public void setMaxTicketCapacity(int maxTicketCapacity) {
+        this.maxTicketCapacity = maxTicketCapacity;
+    }
+
+    public int getNumberOfVendors() {
+        return numberOfVendors;
+    }
+
+    public void setNumberOfVendors(int numberOfVendors) {
+        this.numberOfVendors = numberOfVendors;
+    }
+
+    public int getNumberOfCustomers() {
+        return numberOfCustomers;
+    }
+
+    public void setNumberOfCustomers(int numberOfCustomers) {
+        this.numberOfCustomers = numberOfCustomers;
+    }
+}
